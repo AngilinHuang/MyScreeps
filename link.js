d55f804a591f82c293f4e4c37120033ca2d71620
angilin.huang <angilin@163.com>
2019-05-17 22:41:38 +0800

    将storageLink的传输能量优先级调整到采集点link之下

--- link.js
+++ link.js
@@ -8,10 +8,12 @@
  * Game.rooms["E27S25"].memory.linkFroms=['5ccd4389f331cc50b97ca1a2']
  * Game.rooms["E27S25"].memory.linkTos=['5cd194a18ca9c74b968ec2a8']
  * Game.rooms["E27S25"].memory.linkStorageTo='5ccd3ab22063582490f765de'
+ * Game.rooms["E27S25"].memory.linkStorageFrom='5cde0c35e470435ac71ea750'
  * 
  * Game.rooms["E28S25"].memory.linkFroms=['5ccc5d739b896e0ba42fe85f']
  * Game.rooms["E28S25"].memory.linkTos=['5cd123f1e1b0d7774a40e584']
  * Game.rooms["E28S25"].memory.linkStorageTo='5ccc5020c0d7300bba217ce2'
+ * Game.rooms["E28S25"].memory.linkStorageFrom='5cde73de119e6c0b54b0de9e'
  * 
  * Game.rooms["E27S26"].memory.linkFroms=['5cd0335d0474b64ba28e47d5']
  * Game.rooms["E27S26"].memory.linkStorageTo='5cd02cc317a27d4b8a143c1a'
@@ -19,7 +21,7 @@
  * Game.rooms["E28S26"].memory.linkFroms=['5cd2385af71a733b9e8c7080']
  * Game.rooms["E28S26"].memory.linkStorageTo='5cd21a5c376757774227c31a'
  * 
- * Game.rooms["E29S24"].memory.linkFroms=['5cd67942d2f87745279f4ecc']
+ * 
  * Game.rooms["E29S24"].memory.linkStorageTo='5cd6687092e19f45522a9297'
  * 
  */
@@ -35,7 +37,9 @@ var link = {
     	}
     	
     	if(linkFromIds && linkFromIds.indexOf(linkFrom.id)!=-1){
-    		//注意，如果不填transfer的能量，默认为当前link的全部能量。如果接收者没有足够空间，会导致传输失败。
+    		//如果有采集点link，优先向非storage且能量小于400的link发送能量
+    		//似乎写了能量多少就会等到自己有了这些能量后才传
+    		//写了对方600以下，己方200以上传能量，结果等到己方600才传能量
     		if(linkToIds){
     			const linkTo = linkFrom.pos.findClosestByRange(FIND_STRUCTURES, {
     	            filter: (structure) => {
@@ -45,7 +49,7 @@ var link = {
     		            }
     		    });	
     	    	if(linkTo){
-    	    		if(linkFrom.energy>=200){
+    	    		if(linkFrom.energy>=400){
     	    			linkFrom.transferEnergy(linkTo);
     	    		}
     	    		return;
@@ -73,11 +77,11 @@ var link = {
     		const linkTo = linkFrom.pos.findClosestByRange(FIND_STRUCTURES, {
 	            filter: (structure) => {
 	                return (structure.structureType == STRUCTURE_LINK &&
-	                    		structure.energy < 400 && linkToIds.indexOf(structure.id)!=-1)
+	                    		structure.energy < 100 && linkToIds.indexOf(structure.id)!=-1)
 	                    		;
 		            }
 		    });	
-	    	if(linkTo && linkFrom.energy>=200){
+	    	if(linkTo && linkFrom.energy>=700){
 	    		linkFrom.transferEnergy(linkTo);
 	    	}
     	}
